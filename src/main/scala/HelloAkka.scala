8fe1ce97ec908a7e1a429d2b5d7efa0876129bef
mykhailo.dubovskyi <mdub@ciklum.com>
2018-02-26 22:29:14 +0200

    add helloAkka

---
+++ src/main/scala/HelloAkka.scala
@@ -0,0 +1,18 @@
+import akka.actor.{Actor, ActorSystem, Props}
+
+case class  WhoToGreet(who: String)
+
+class Greeter extends Actor {
+
+   def receive = {
+     case WhoToGreet(who) => println(s"Hello $who")
+   }
+}
+
+
+object HelloAkka extends App {
+
+  val  system = ActorSystem("Hello-Akka")
+  val greeter = system.actorOf(Props[Greeter],"greeter")
+  greeter ! WhoToGreet("akka")
+}
